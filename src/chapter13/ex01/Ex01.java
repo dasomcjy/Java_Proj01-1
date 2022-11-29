4334720d0afd879b90ee2a5985a2c23bad3aa6c0
Jiyoon Choi <dasomcjy@naver.com>
2022-11-29 13:42:39 +0900

    11/29 오전

---
+++ src/chapter13/ex01/Ex01.java
@@ -0,0 +1,46 @@
+package chapter13.ex01;
+
+class C {
+	int a = 5;
+	int b = 4;
+	
+	int add (int a, int b) {
+		return a + b ;		
+	}
+	int diff (int a, int b) {
+		return a-b;
+	}
+	int mul (int a, int b) {
+		return a * b;
+	}
+	double div (int a, int b) {
+		return (double)a / (double) b;
+	}
+	
+	//이너클래스 : Outer 클래스의 메소드를 활용해서 기능을 구현
+	class D {
+		void abc () {
+			System.out.println("두 수의 합은" + add(a , b));
+			System.out.println("두 수의 뺄셈은" + diff(a , b));
+			System.out.println("두 수의 곱셈은" + mul(a , b));
+			System.out.println("두 수의 나눗셉은" + div(a , b));
+			
+		}
+	}
+	
+}
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		//1. 외부객체 생성
+		C c = new C();
+		
+		//2. 내부 객체 생성
+		C.D d = c.new D();
+		
+		d.abc(); 
+
+	}
+
+}
